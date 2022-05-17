cfa3d0a2112db28d572699929483829326226244
Hitesh Chalise <hiteshchalise10@gmail.com>
2022-05-17 08:31:04 +0545

    AnecdoteList is separated to its own component

---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,28 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { castVote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+
+  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+  const dispatch = useDispatch()
+  const vote = (id) => {
+    dispatch(castVote(id))
+  }
+
+  return (<>
+    <h2>Anecdotes</h2>
+    {anecdotes.map(anecdote =>
+      <div key={anecdote.id}>
+        <div>
+          {anecdote.content}
+        </div>
+        <div>
+          has {anecdote.votes}
+          <button onClick={() => vote(anecdote.id)}>vote</button>
+        </div>
+      </div>
+    )}
+  </>)
+}
+
+export default AnecdoteList
